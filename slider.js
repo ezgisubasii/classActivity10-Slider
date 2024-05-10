e9cdbbcd217ff4d66c7ba1695cdfc7b46f28cecd
ezgisubasii <120321455+ezgisubasii@users.noreply.github.com>
2024-05-10 17:16:21 +0300

    add files

---
+++ slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	$("#slider").bxSlider({
+		auto: true,
+		slideWidth: 250,
+		slideMargin: 10,
+		randomStart: true,
+		speed: 300,
+		moveSlides: 1,
+		pager: true,
+		pagerType: "short",
+        captions: true,
+		touchEnabled: false,
+		
+	});
+
+});
+
